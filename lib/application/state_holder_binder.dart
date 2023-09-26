4da9d31916c0c0e4d82e792af001dddb43a6eb32
FarisHollow <mirfaris79@gmail.com>
2023-09-26 11:29:28 +0600

    Api integration start and email verify done

--- lib/application/state_holder_binder.dart
+++ lib/application/state_holder_binder.dart
@@ -1,9 +1,14 @@
 import 'package:e_commerce/presentation/state_holders/main_bottom_nav_controller.dart';
 import 'package:get/get.dart';
+import 'package:e_commerce/presentation/state_holders/email_verification_controller.dart';
+
+
 
 class StateHolderBinder extends Bindings {
   @override
   void dependencies() {
     Get.put(MainBottomNavController());
+    Get.put(EmailVerificationController());
+
   }
 }
